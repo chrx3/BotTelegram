25d1f307f91f3b77d24b8ca2cf3701533141d25f
chrx3 <chris.alc.13@gmail.com>
2022-07-11 22:05:36 -0400

    Cambio de api de telebot a aiogram

--- webscraping.py
+++ webscraping.py
@@ -1,16 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-import telebot
-# from webscraping import precioDeseado
-# from webscraping import precioInicial
-# from webscraping import price, title, links, catalogo
-import requests
+# import telebot
+import time
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token='5446917926:AAGNVWK7cjTyy5Zt4rq-YOyy_aYfdLC3bvs')
+dp = Dispatcher(bot)
         
 
-#web scraping
-url = ("https://simple.ripley.cl/tecno/computacion/notebooks?source=search&term=notebooks&s=mdco")
+# web scraping
+url = "https://simple.ripley.cl/tecno/computacion/notebooks?source=search&term=notebooks&s=mdco"
 r = requests.get(url)
-soup = BeautifulSoup(r.content,"html.parser")
+soup = BeautifulSoup(r.content, "html.parser")
 # links = soup.find_all('a',{'class': 'catalog-product-item'},href=True)
 
 # for i in catalogo:
@@ -19,30 +20,32 @@ soup = BeautifulSoup(r.content,"html.parser")
 # for href in soup.find_all('a',{'class': 'catalog-product-item'},href=True):
 #     links = href.get('href')
 
-bot = telebot.TeleBot('5446917926:AAGNVWK7cjTyy5Zt4rq-YOyy_aYfdLC3bvs')
-catalogo = soup.find_all("div", {"class":"catalog-product-item"})
+#bot = telebot.TeleBot('5446917926:AAGNVWK7cjTyy5Zt4rq-YOyy_aYfdLC3bvs')
+catalogo = soup.find_all("div", {"class": "catalog-product-item"})
 
-#bot de telegram
-@bot.message_handler(commands=['producto'])
-def pedirProducto(msg):
-    title  = soup.find('div',{'class':'catalog-product-details__name'}).text
-    price = soup.find('li',{'class':'catalog-prices__offer-price'}).text
-    links = soup.find('a',{'class': 'catalog-product-item__container'}, href=True)
-    bot.reply_to(msg,"Mira esto:\n" + title+ '\n' + f"  {'Solo a : '+str(price)}\n" + 'https://simple.ripley.cl' + links.get('href') )
-    
-@bot.message_handler(commands=['productos'])
-def pedirProducto(msg):
-    for i in catalogo:
-        title  = i.find('div',{'class':'catalog-product-details__name'}).text
-        price = i.find('li',{'class':'catalog-prices__offer-price'}).text
-        links = i.find('a',{'class': 'catalog-product-item__container'}, href=True)
+# bot de telegram
+@dp.message_handler(commands=['ping'])
+async def send_ping(message: types.Message):
+    await message.reply("Toi vivo")
 
-        bot.reply_to(msg,"Mira esto:\n" + title+ '\n' + f"  {'Solo a : '+str(price)}\n" + 'https://simple.ripley.cl' + links.get('href'))
-     
-        
-   
+@dp.message_handler(commands=['producto'])
+async def pedirproducto(message: types.Message):
+    title = soup.find('div', {'class': 'catalog-product-details__name'}).text
+    price = soup.find('li', {'class': 'catalog-prices__offer-price'}).text
+    links = soup.find('a', {'class': 'catalog-product-item__container'}, href=True)
+    await message.reply("Mira esto: \n" + title + '\n' + f"  {'Solo a : '+str(price)}\n" + 'https://simple.ripley.cl' + links.get('href'))
     
-bot.infinity_polling() 
+@dp.message_handler(commands=['productos'])
+async def pedirproductos(message: types.Message):
+    cant = int(input("cuantos productos?"))
+    for i in range(cant):
+        time.sleep(0.3)
+        title  = soup.find('div', {'class': 'catalog-product-details__name'}).text
+        price = soup.find('li', {'class': 'catalog-prices__offer-price'}).text
+        links = soup.find('a', {'class': 'catalog-product-item__container'}, href=True)
+
+        await message.reply("Mira esto:\n" + title + '\n' + f"  {'Solo a : '+str(price)}\n" + 'https://simple.ripley.cl' + links.get('href'))
+executor.start_polling(dp)
  
 
 
